c48cbda741f3c478c5424d4ef430fc45eac98706
swomf <swomf@proton.me>
2024-12-17 11:36:25 -0500

    2024-advent: solve 03.2

--- comp/2024-advent/03/main.cpp
+++ comp/2024-advent/03/main.cpp
@@ -7,12 +7,20 @@ using namespace std;
 #define lli long long int
 
 int main() {
-  int a, b;
+  int a, b, enabled = 1;
   lli total = 0;
   string line;
   while (getline(cin, line)) {
+    if (line == "do()") {
+      enabled = 1;
+      continue;
+    } else if (line == "don't()") {
+      enabled = 0;
+      continue;
+    }
     sscanf(line.c_str(), "mul(%d,%d)", &a, &b);
-    total += (a * b);
+    if (enabled)
+      total += (a * b);
   }
   cout << total;
   return 0;
